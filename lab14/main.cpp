a200e2e56df8bd6373ff990b605417be43949035
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-09-16 12:07:54 -0700

    Initial Commit

---
+++ lab14/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab14
+//
+//  Created by Chatchapong Thuaypha on 9/16/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
