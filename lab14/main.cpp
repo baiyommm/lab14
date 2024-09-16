d31f9a39567b575ab8d07f48b6b1aa25087d3e6f
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-09-16 13:44:47 -0700

    Completed the program using a while loop asking the user to input a number until guessed correctly. Used the combined  rand() and srand() function to generate a random number for the user to guess.

--- lab14/main.cpp
+++ lab14/main.cpp
@@ -1,14 +1,32 @@
-//
-//  main.cpp
-//  lab14
-//
-//  Created by Chatchapong Thuaypha on 9/16/24.
-//
-
 #include <iostream>
+#include <cstdlib> // rand() and srand() function
+#include <ctime>   // time function
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    // code for random number generator
+    srand(static_cast<unsigned int>(time(0)));
+
+    // code to generate a random number between 1 and 100
+    int randomNumber = rand() % 100 + 1;
+
+    int guessNumber;  // stores the user's guess
+
+    cout << "Please guess a number (between 1 and 100): " << endl;
+
+    // While loop to keep asking the user until they guess correctly
+    while (true) {
+        cin >> guessNumber;  // inputs the user's guess
+
+        if (guessNumber == randomNumber) {
+            cout << "You guessed correctly! That is the secret number!" << endl;
+            break;  // Exit the loop when the correct guess is entered
+        }
+        else {
+            cout << "You guessed wrong! Try again!" << endl;
+        }
+    }
+
     return 0;
 }
